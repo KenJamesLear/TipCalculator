8199484f9e796d8766f63cf03c8636594e4e8893
KenJamesLear <kenneth.lear@scranton.edu>
2016-07-13 21:19:52 -0400

    First Commit

--- app/src/main/java/lear/scranton/edu/tipcalculator/MainActivity.java
+++ app/src/main/java/lear/scranton/edu/tipcalculator/MainActivity.java
@@ -1,32 +1,14 @@
 package lear.scranton.edu.tipcalculator;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Button;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.TextView;
-import android.widget.Button;
-import android.app.Activity;
 import android.content.Intent;
-import android.widget.Toast;
 import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
